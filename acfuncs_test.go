a6804328aa673f8401160adc4d0bab6d706d9419
choueric <zhssmail@gmail.com>
2017-09-05 11:08:18 +0800

    add testcase for acfuncs

---
+++ acfuncs_test.go
@@ -0,0 +1,32 @@
+package main
+
+import "testing"
+
+type TestPair struct {
+	input     string
+	expect    string
+	dirPrefix string
+}
+
+func Test_getPath(t *testing.T) {
+	testPair := []TestPair{
+		{"a", currentDir, ""},
+		{"./", currentDir, "."},
+		{".", currentDir, ""},
+		{"./a", currentDir, "."},
+		{"/u", "/", "/"},
+		{"/usr", "/", "/"},
+		{"/usr/", "/usr", "/usr"},
+		{"add ", currentDir, ""},
+	}
+
+	for i, p := range testPair {
+		output, dirPrefix := getDir(p.input)
+		if output != p.expect {
+			t.Errorf("[%d] (%s -> %s) [%s]", i, p.input, p.expect, output)
+		}
+		if dirPrefix != p.dirPrefix {
+			t.Errorf("[%d] prefix (%s -> %s) [%s]", i, p.input, p.dirPrefix, dirPrefix)
+		}
+	}
+}
